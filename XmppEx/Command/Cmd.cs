6e8493af6378958b9679c29e80a1bb6e2b5243e8
foxundermoon <fox@vvfox.com>
2015-05-13 14:26:18 +0800

    add all file

---
+++ XmppEx/Command/Cmd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxundermoonLib.XmppEx.Command
+{
+   public class Cmd
+    {
+       public const string GetOnlineUsers = "GetOnlineUsers";
+       public const string GetOnlineUsersResponse = "GetOnlineUsersResponse";
+       public const string GetUserTask = "GetUserTask";
+       public const string GetUserTaskResponse = "GetUserTaskResponse";
+       public const string GetUpLine = "GetUpLine";
+       public const string GetUpLineResponse = "GetUpLineResponse";
+       public const string GetDownLine = "GetDownLine";
+       public const string GetDownLineResponse = "GetDownLineResponse";
+    }
+}
