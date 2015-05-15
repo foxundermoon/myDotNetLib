458277c64d05c26568907c675ebf026547157c6c
qupeng <qupeng@qq.com|>
2015-05-15 15:16:22 +0800

    fix the cmd

--- XmppEx/Command/Cmd.cs
+++ XmppEx/Command/Cmd.cs
@@ -8,17 +8,27 @@ namespace FoxundermoonLib.XmppEx.Command
 {
    public class Cmd
     {
-       public const String GetOnlineUsers = "GetOnlineUsers";
-       public const String GetOnlineUsersResponse = "GetOnlineUsersResponse";
-       public const String GetUserTask = "GetUserTask";
-       public const String GetUserTaskResponse = "GetUserTaskResponse";
+       public const String GetOnlineUsers = "GetOnlineUsers"; //向服务获取全部在线用户
+       public const String GetOnlineUsersResponse = "GetOnlineUsersResponse";  //接受全部在线任务
+       //public const String GetUserTask = "GetUserTask";
+       //public const String GetUserTaskResponse = "GetUserTaskResponse";
        //public const String GetUpLine = "GetUpLine";
        //public const String GetUpLineResponse = "GetUpLineResponse";
        //public const String GetDownLine = "GetDownLine";
        //public const String GetDownLineResponse = "GetDownLineResponse";
-       public const String UserLogin = "UserLogin";
-       public const String OnLineAtOtherPlace = "OnLineAtOtherPlace";
-       public const String UserOffLine = "UserOffLine";
-       public const String ErrorMessage = "ErrorMessage";
+       public const String UserLogin = "UserLogin";  //有新用户登录
+       public const String OnLineAtOtherPlace = "OnLineAtOtherPlace"; //用户被顶下线
+       public const String UserOffLine = "UserOffLine"; //用户离线
+       public const String ErrorMessage = "ErrorMessage";  //错误相关消息
+
+       public const String SendTask = "SendTask";   // 下发任务
+       public const String SendTaskResponse = "SendTaskResponse";  //收到下发任务
+       public const String DataTable = "DataTable";  //数据库/表相关操作
+
+
+
+
+
+
     }
 }
